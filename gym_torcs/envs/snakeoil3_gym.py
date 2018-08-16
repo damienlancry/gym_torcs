d881ff5201594c0f271ec90bbaeab415e2f9ee0f
Damien Lancry <damien.lancry17@imperial.ac.uk>
2018-08-16 14:01:44 +0100

    comment

--- gym_torcs/envs/snakeoil3_gym.py
+++ gym_torcs/envs/snakeoil3_gym.py
@@ -1,56 +1,5 @@
 #!/usr/bin/python
-# snakeoil.py
-# Chris X Edwards <snakeoil@xed.ch>
-# Snake Oil is a Python library for interfacing with a TORCS
-# race car simulator which has been patched with the server
-# extentions used in the Simulated Car Racing competitions.
-# http://scr.geccocompetitions.com/
-#
-# To use it, you must import it and create a "drive()" function.
-# This will take care of option handling and server connecting, etc.
-# To see how to write your own client do something like this which is
-# a complete working client:
-# /-----------------------------------------------\
-# |#!/usr/bin/python                              |
-# |import snakeoil                                |
-# |if __name__ == "__main__":                     |
-# |    C= snakeoil.Client()                       |
-# |    for step in xrange(C.maxSteps,0,-1):       |
-# |        C.get_servers_input()                  |
-# |        snakeoil.drive_example(C)              |
-# |        C.respond_to_server()                  |
-# |    C.shutdown()                               |
-# \-----------------------------------------------/
-# This should then be a full featured client. The next step is to
-# replace 'snakeoil.drive_example()' with your own. There is a
-# dictionary which holds various option values (see `default_options`
-# variable for all the details) but you probably only need a few
-# things from it. Mainly the `trackname` and `stage` are important
-# when developing a strategic bot.
-#
-# This dictionary also contains a ServerState object
-# (key=S) and a DriverAction object (key=R for response). This allows
-# you to get at all the information sent by the server and to easily
-# formulate your reply. These objects contain a member dictionary "d"
-# (for data dictionary) which contain key value pairs based on the
-# server's syntax. Therefore, you can read the following:
-#    angle, curLapTime, damage, distFromStart, distRaced, focus,
-#    fuel, gear, lastLapTime, opponents, racePos, rpm,
-#    speedX, speedY, speedZ, track, trackPos, wheelSpinVel, z
-# The syntax specifically would be something like:
-#    X= o[S.d['tracPos']]
-# And you can set the following:
-#    accel, brake, clutch, gear, steer, focus, meta
-# The syntax is:
-#     o[R.d['steer']]= X
-# Note that it is 'steer' and not 'steering' as described in the manual!
-# All values should be sensible for their type, including lists being lists.
-# See the SCR manual or http://xed.ch/help/torcs.html for details.
-#
-# If you just run the snakeoil.py base library itself it will implement a
-# serviceable client with a demonstration drive function that is
-# sufficient for getting around most tracks.
-# Try `snakeoil.py --help` to get started.
+# Inspired by snakeoil.py by Chris Edward
 
 # for Python3-based torcs python robot client
 import socket
